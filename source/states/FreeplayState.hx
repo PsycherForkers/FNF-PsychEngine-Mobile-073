687f9425645e8f1f373fdc27f28ca6cd6b079999
DafHubAltAccount <128113481+DafHubAltAccount@users.noreply.github.com>
2024-02-07 22:29:33 -0300

    Update FreeplayState.hx

--- source/states/FreeplayState.hx
+++ source/states/FreeplayState.hx
@@ -86,7 +86,7 @@ class FreeplayState extends MusicBeatState
 				addSong(song[0], i, song[1], FlxColor.fromRGB(colors[0], colors[1], colors[2]));
 			}
 		}
-		Mods.loadTopMod();
+		WeekData.setDirectoryFromWeek();
 
 		bg = new FlxSprite().loadGraphic(Paths.image('menuDesat'));
 		bg.antialiasing = ClientPrefs.data.antialiasing;

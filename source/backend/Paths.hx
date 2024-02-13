2dfe5ea349207941b2a6582df9d1b44150acb3b5
DafHubAltAccount <128113481+DafHubAltAccount@users.noreply.github.com>
2024-02-13 18:03:43 -0300

    Update Paths.hx

--- source/backend/Paths.hx
+++ source/backend/Paths.hx
@@ -416,11 +416,7 @@ class Paths
 	}
 
 	inline static public function formatToSongPath(path:String) {
-		var invalidChars = ~/[~&\\;:<>#]/;
-		var hideChars = ~/[.,'"%?!]/;
-
-		var path = invalidChars.split(path.replace(' ', '-')).join("-");
-		return hideChars.split(path).join("").toLowerCase();
+		return path.toLowerCase().replace(' ', '-');
 	}
 
 	public static var currentTrackedSounds:Map<String, Sound> = [];
